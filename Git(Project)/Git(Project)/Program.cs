96b7923718b7ef8e85dac7cb099686c94a15ace0
asp <asp@Aptechnk.edu>
2019-11-06 16:03:07 +0500

    Updated by Idrees on 6/11/2019

---
+++ Git(Project)/Git(Project)/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Git_Project_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+            Console.WriteLine("This is my First Git Project");
+            Console.ReadKey();
+        }
+    }
+}
